8e377b26186c0058f40ba287f7d9379a75e7b18b
dubbersthehoser <dubbersthehoser@hotmail.com>
2025-01-14 13:12:51 -0800

    ship and its movement system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -11,11 +12,22 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    drawables = pygame.sprite.Group()
+    updateables = pygame.sprite.Group()
+
+    Player.containers = (drawables, updateables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for updatable in updateables:
+            updatable.update(dt)
         screen.fill((0,0,0))
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)
 
